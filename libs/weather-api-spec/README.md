0144fb575cd57bb7e9fa1ff40b57e77752374465
Connor Sullivan <dev.connorjs@gmail.com>
2023-10-29 21:49:33 -0400

    example api spec w/ typespec; using weather service; basic hello for now

---
+++ libs/weather-api-spec/README.md
@@ -0,0 +1,5 @@
+# weather-api-spec
+
+API specification for an example Weather Service API using [TypeSpec].
+
+[typespec]: https://microsoft.github.io/typespec/
--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
 	// Note: Switch to array syntax if concurrency issues appear
-	"*.{cjs,css,cts,graphql,html,js,json,jsonc,jsx,md,pcss,scss,ts,tsx}": `prettier --write`,
+	"*.{cjs,css,cts,graphql,html,js,json,jsonc,jsx,md,pcss,scss,ts,tsp,tsx}": `prettier --write`,
 	"*.{cjs,cts,graphql,html,js,json,jsonc,jsx,ts,tsx}": `eslint --fix`,
 };
